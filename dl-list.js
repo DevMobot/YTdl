53c4c69161ce067458aa030e3cd8ebd69f4e911f
Mobot-star <82704481+Mobot2121@users.noreply.github.com>
2022-04-13 18:03:39 +0530

    v2

--- dl-list.js
+++ dl-list.js
@@ -1,6 +1,7 @@
 const ytdl = require("ytdl-core");
 const yts = require("yt-search");
 const fs = require("fs");
+const config = require("./config.json");
 
 const dl_list = [
     ""
@@ -21,11 +22,34 @@ const main = async (searchFor) => {
             quality: "highestaudio"
         }).pipe(dl);
 
+        /*
         await dl.on("finish", async () => {
             dl.close();
             console.log("Downloded " + title);
             return true;
-        });
+        }); */
+
+        await dl.on("finish", async () => {
+            await dl.close();
+            await console.log("Downloaded! Converting into mp3...");
+
+                await convert(title, async (out) => {
+
+                if (out == "ERROR") process.exit(1);
+
+                await del(title);
+
+                const tags = {
+                    artist: results.videos[0].author.name+"".substring(0,29),
+                    album: "YT"
+                }
+
+                await setTags(title, tags, (o) => { return });
+
+                await console.log(out);
+                await main();
+            });
+        })
 
     }).catch(e => {
         console.log("Could not download the video... \nError: " + e.message);
