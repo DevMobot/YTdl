c571f23dfdd516c66e0ea929d74ad122724ab9d7
Mobot-star <82704481+Mobot2121@users.noreply.github.com>
2022-06-21 17:50:38 +0530

    k

--- converter.js
+++ converter.js
@@ -2,7 +2,7 @@ const nodeCmd = require('node-cmd');
 const config = require("./config.json");
 
 const convert = async (video, out) => {
-    nodeCmd.run(`ffmpeg -i ${config.dl_path_mp4}/${video}.mp4 -b:a ${config.bitrate_mp3} ${config.dl_path_mp3}/${video}.mp3`, (err, data, stderr) => {
+    nodeCmd.run(`ffmpeg -i ${config.dl_path_mp4}${video}.mp4 -b:a ${config.bitrate_mp3} ${config.dl_path_mp3}${video}.mp3`, (err, data, stderr) => {
         if (!err) {
             out("Completed!");
         } else {
--- dl-list.js
+++ dl-list.js
@@ -19,6 +19,8 @@ const main = async () => {
     const dl_playlist = async (id, pn) => {
         const data = await yts.search({ listId: id });
 
+        
+
         data.videos.forEach(async v => {
             const title = v.title.replace(/[^\w\s]/gi, '').split(/ +/).join("_");
             const dl = fs.createWriteStream(config.playlist_dl_path + pn+"/" + title + ".mp4");
@@ -70,49 +72,7 @@ const main = async () => {
         await c(a);
     });
  
-    /*
-    await yts.search(searchFor).then(async results => {
 
-        const videoURL = results.videos[0].url;
-        const title = results.videos[0].title.replace(/[^\w\s]/gi, '');
-        await console.log("Downloading " + title);
-
-        const dl = fs.createWriteStream(config.playlist_dl_path + title + '.mp4');
-
-        const stream = await ytdl(videoURL,  {
-            highWaterMark: 33554432,
-            quality: "highestaudio"
-        }).pipe(dl);
-
-        await dl.on("finish", async () => {
-
-            await dl.close();
-            
-            await console.log("Downloaded! Converting into mp3...");
-
-            await convert(title, async (out) => {
-
-                if (out == "ERROR") process.exit(1);
-
-                await del(title);
-
-                const tags = {
-                    artist: results.videos[0].author.name+"".substring(0,29),
-                    album: "YT"
-                }
-
-                await setTags(title, tags, (o) => { return });
-
-                await console.log(out);
-                await main();
-            });
-        })
-
-    }).catch(e => {
-        console.log("Could not download the video... \nError: " + e.message);
-        return false;
-    });
-    */
 }
 
 main()
